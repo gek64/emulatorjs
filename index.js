f2b3cabb564a51fce3f6cee2d156f670ccb20570
cekys <10346089+cekys@users.noreply.github.com>
2020-08-19 07:41:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,19 +6,27 @@ const cors = require('cors')
 // 获取自定义的mock中间件
 const mock = require('./mock')
 
-let startServer = function (url, template) {
-    let app = express()
-    // 所有路由设置为可跨域访问
-    app.use(cors())
+const serverPort = 3000
+
+let app = express()
+
+app.use(cors())
+
+
+let setServer = function (url, template) {
     app.use(url, function (req, res, next) {
         res.json(mock.newData(template))
         next()
     })
-    let server = app.listen('3000', function () {
-        console.log('mock data server visit: http://localhost:' + server.address().port + url)
+}
+
+let startServer = function () {
+    let server = app.listen(serverPort, function () {
+        console.log('mock server started')
     })
 }
 
 module.exports = {
+    setServer,
     startServer
 }
